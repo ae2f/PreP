165b204d1d8bc73a0e54152166c2affae5098dbe
YuiSanae2f <mincraft20046@gmail.com>
2024-11-24 06:17:02 +0900

    Rel-v0.3.1

--- In.py
+++ In.py
@@ -1,32 +1,39 @@
-import os
+from pathlib import Path
 from . import Env
-import pathlib
 import collections.abc as types
 
 RD_INVALID: tuple[str, bool] = ('', Env.EXCLUDED)
-RD_GOT_NOTHING : tuple[str, bool] = ('', Env.INCLUDED)
+RD_GOT_NOTHING: tuple[str, bool] = ('', Env.INCLUDED)
 
 PF_T = tuple[str, bool]
 
 def Rd(l: str) -> PF_T:
-    if(l.startswith('#include')):
+    if l.startswith('#include'):
         TAR: str = l.replace(' ', '')
-        PATH_GET: str = TAR[len('#include')::]
-        FLAG : bool
-        match(PATH_GET[0]):
-            case '<': FLAG = Env.INCLUDED; pass
-            case '"': FLAG = Env.EXCLUDED; pass
-            case _: return RD_INVALID
-        RET_STR : str = PATH_GET[1:len(PATH_GET) - 2]
-        return (RET_STR, FLAG)
+        PATH_GET: str = TAR[len('#include')::].replace(' ', '')
+        FLAG: bool
+        RET_STR: str
 
-    else: return RD_GOT_NOTHING
+        match PATH_GET[0]:
+            case '<': 
+                FLAG = Env.INCLUDED
+                RET_STR = PATH_GET.split('<')[1].split('>')[0]
+            case '"': 
+                FLAG = Env.EXCLUDED
+                RET_STR = PATH_GET.split('"')[1]
+            case _: 
+                return RD_INVALID
+
+        return (RET_STR, FLAG)
+    else: 
+        return RD_GOT_NOTHING
 
 def Fnd(fname: str, include: types.Iterable[str]) -> tuple[str, list[str]]:
     for PATH in include:
-        if(not os.path.isfile(PATH + "/" + fname)):
+        path_obj = Path(PATH) / fname
+        if not path_obj.is_file():
             continue
-        with open(PATH + '/' + fname, 'r') as F:
-            return (os.path.dirname(PATH + '/' + fname), F.readlines())
+        with path_obj.open('r') as F:
+            return (path_obj.parent.as_posix(), F.readlines())
 
     return ('', [])
